f82f377d0e426b483ff0f3b2014da101d639d014
ChrichriR <chriss_s@hotmail.fr>
2022-05-30 10:35:14 +0200

    modifications about the layout, creation of a nav components and make a litlle bit of css

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-import Link from "next/link";
 import Head from "next/head";
+import NavBar from "./Nav";
+// import style from "./Layout.module.css";
 
 export default function Layout({ children, pageTitle }) {
   return (
@@ -10,25 +10,7 @@ export default function Layout({ children, pageTitle }) {
         <meta name="description" content="Atempo for practitioners" />
         <title>{pageTitle}</title>
       </Head>
-      <header>
-        <nav>
-          <Link href="/">
-            <a>Accueil</a>
-          </Link>
-          <Link href="/dashboard">
-            <a>Tableau de bord</a>
-          </Link>
-          <Link href="/clients">
-            <a>Clients</a>
-          </Link>
-          <Link href="/sessions">
-            <a>Séances</a>
-          </Link>
-          <Link href="/profile">
-            <a>Profile</a>
-          </Link>
-        </nav>
-      </header>
+      <NavBar />
       {children}
     </>
   );
