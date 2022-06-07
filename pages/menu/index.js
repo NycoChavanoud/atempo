8906cbdee3f88e3600aa09481320085179ec5125
ChrichriR <chriss_s@hotmail.fr>
2022-06-07 07:00:52 +0200

    encapsulate the links in the menu page

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -10,98 +10,98 @@ export default function MobileMenu() {
         <img className={style.user} src="/images/add1.png" alt="add a user" />
         <div className={style.links}>
           <button className={style.button}>Créer une séance</button>
-          <div className={style.link}>
-            <div className={style.icon}>
-              <Image
-                priority
-                height={40}
-                width={45}
-                src="/images/dashboard.png"
-                alt="dashboard"
-              />
-            </div>
-            <Link data-cy="dashboardBtn" href="/dashboard">
+          <Link data-cy="dashboardBtn" href="/dashboard">
+            <div className={style.link}>
+              <div className={style.icon}>
+                <Image
+                  priority
+                  height={40}
+                  width={45}
+                  src="/images/dashboard.png"
+                  alt="dashboard"
+                />
+              </div>
               <a className={style.text}>Tableau de bord</a>
-            </Link>
-            <div className={style.arrow}>
-              <Image
-                priority
-                height={20}
-                width={10}
-                src="/images/right_arrow.png"
-                alt="flèche vers la droite"
-              />
+              <div className={style.arrow}>
+                <Image
+                  priority
+                  height={20}
+                  width={10}
+                  src="/images/right_arrow.png"
+                  alt="flèche vers la droite"
+                />
+              </div>
             </div>
-          </div>
-          <div className={style.link}>
-            <div className={style.icon}>
-              <Image
-                priority
-                height={40}
-                width={45}
-                src="/images/clients.png"
-                alt="hearts"
-              />
-            </div>
-            <Link href="/clients">
+          </Link>
+          <Link href="/clients">
+            <div className={style.link}>
+              <div className={style.icon}>
+                <Image
+                  priority
+                  height={40}
+                  width={45}
+                  src="/images/clients.png"
+                  alt="hearts"
+                />
+              </div>
               <a className={style.text}>Clients</a>
-            </Link>
-            <div className={style.arrow}>
-              <Image
-                priority
-                height={20}
-                width={10}
-                src="/images/right_arrow.png"
-                alt="flèche vers la droite"
-              />
+              <div className={style.arrow}>
+                <Image
+                  priority
+                  height={20}
+                  width={10}
+                  src="/images/right_arrow.png"
+                  alt="flèche vers la droite"
+                />
+              </div>
             </div>
-          </div>
-          <div className={style.link}>
-            <div className={style.icon}>
-              <Image
-                priority
-                height={50}
-                width={50}
-                src="/images/stars.png"
-                alt="stars"
-              />
-            </div>
-            <Link href="/seances">
+          </Link>
+          <Link href="/seances">
+            <div className={style.link}>
+              <div className={style.icon}>
+                <Image
+                  priority
+                  height={50}
+                  width={50}
+                  src="/images/stars.png"
+                  alt="stars"
+                />
+              </div>
               <a className={style.text}>Séances</a>
-            </Link>
-            <div className={style.arrow}>
-              <Image
-                priority
-                height={20}
-                width={10}
-                src="/images/right_arrow.png"
-                alt="flèche vers la droite"
-              />
+              <div className={style.arrow}>
+                <Image
+                  priority
+                  height={20}
+                  width={10}
+                  src="/images/right_arrow.png"
+                  alt="flèche vers la droite"
+                />
+              </div>
             </div>
-          </div>
-          <div className={style.link}>
-            <div className={style.icon}>
-              <Image
-                priority
-                height={40}
-                width={40}
-                src="/images/profil.png"
-                alt="user's profiles"
-              />
-            </div>
-            <Link href="/profile">
+          </Link>
+          <Link href="/profile">
+            <div className={style.link}>
+              <div className={style.icon}>
+                <Image
+                  priority
+                  height={40}
+                  width={40}
+                  src="/images/profil.png"
+                  alt="user's profiles"
+                />
+              </div>
               <a className={style.text}>Profil</a>
-            </Link>
-            <div className={style.arrow}>
-              <Image
-                priority
-                height={20}
-                width={10}
-                src="/images/right_arrow.png"
-                alt="flèche vers la droite"
-              />
+              <div className={style.arrow}>
+                <Image
+                  priority
+                  height={20}
+                  width={10}
+                  src="/images/right_arrow.png"
+                  alt="flèche vers la droite"
+                />
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </Layout>
