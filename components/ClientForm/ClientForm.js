33b5a63e5608fa090e96fef6dd9fd8892058b9bd
ChrichriR <chriss_s@hotmail.fr>
2022-06-16 15:50:31 +0200

    changes css

--- components/ClientForm/ClientForm.js
+++ components/ClientForm/ClientForm.js
@@ -32,74 +32,73 @@ export default function ClientForm() {
   return (
     <>
       <form className={style.form} onSubmit={handleSubmit}>
-        <label htmlFor="prénom">
-          <input
-            data-cy="firstname"
-            className={style.input}
-            value={firstname}
-            type="text"
-            id="firstname"
-            placeholder="Prénom"
-            required
-            onChange={(e) => setFirstname(e.target.value)}
-          />
-        </label>
-        <label htmlFor="nom">
-          <input
-            data-cy="lastname"
-            className={style.input}
-            value={lastname}
-            type="text"
-            id="lastname"
-            placeholder="Nom"
-            required
-            onChange={(e) => setLastname(e.target.value)}
-          />
-        </label>
-        <label htmlFor="email">
-          <input
-            data-cy="email"
-            className={style.input}
-            value={email}
-            type="email"
-            id="email"
-            placeholder="email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label htmlFor="téléphone">
-          <input
-            className={style.input}
-            value={phoneNumber}
-            type="number"
-            id="phoneNumber"
-            placeholder="Téléphone"
-            onChange={(e) => setPhoneNumber(e.target.value)}
-          />
-        </label>
-        <label htmlFor="probématique">
-          <input
-            data-cy="problematic"
-            className={style.input}
-            value={problematic}
-            type="text"
-            id="problematic"
-            placeholder="Problématique"
-            required
-            onChange={(e) => setProblematic(e.target.value)}
-          />
-        </label>
-        <label htmlFor="adresse">
-          <input
-            className={style.input}
-            value={adress}
-            type="text"
-            id="adress"
-            placeholder="Adresse"
-            onChange={(e) => setAdress(e.target.value)}
-          />
-        </label>
+        <label htmlFor="prénom"> </label>
+        <input
+          data-cy="firstname"
+          className={style.input}
+          value={firstname}
+          type="text"
+          id="firstname"
+          placeholder="Prénom"
+          required
+          onChange={(e) => setFirstname(e.target.value)}
+        />
+
+        <label htmlFor="nom"> </label>
+        <input
+          data-cy="lastname"
+          className={style.input}
+          value={lastname}
+          type="text"
+          id="lastname"
+          placeholder="Nom"
+          required
+          onChange={(e) => setLastname(e.target.value)}
+        />
+
+        <label htmlFor="email"> </label>
+        <input
+          data-cy="email"
+          className={style.input}
+          value={email}
+          type="email"
+          id="email"
+          placeholder="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="téléphone"> </label>
+        <input
+          className={style.input}
+          value={phoneNumber}
+          type="number"
+          id="phoneNumber"
+          placeholder="Téléphone"
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+
+        <label htmlFor="probématique"> </label>
+        <input
+          data-cy="problematic"
+          className={style.input}
+          value={problematic}
+          type="text"
+          id="problematic"
+          placeholder="Problématique"
+          required
+          onChange={(e) => setProblematic(e.target.value)}
+        />
+
+        <label htmlFor="adresse"> </label>
+        <input
+          className={style.input}
+          value={adress}
+          type="text"
+          id="adress"
+          placeholder="Adresse"
+          onChange={(e) => setAdress(e.target.value)}
+        />
 
         <button
           data-cy="submitBtn"
