1725b9fcbb461d5b4f56f3f623572b3c6f08372d
fflachat <florent.flachat@gmail.com>
2022-06-17 10:34:49 +0200

    change some css

--- pages/seances/[id]/index.js
+++ pages/seances/[id]/index.js
@@ -5,7 +5,7 @@ import styles from "../../../styles/Seances.module.css";
 import SeanceDetails from "../../../components/SeanceDetails/SeanceDetails";
 import AssociatedClients from "../../../components/AssociatedClients/AssociatedClients";
 import { useRouter } from "next/router";
-import WhiteBurger from "../../../components/WhiteBurger/WhiteBurger";
+import WaveWhiteBurger from "../../../components/WaveWhiteBurger/WaveWhiteBurger";
 
 export default function Seance() {
   const router = useRouter();
@@ -13,14 +13,20 @@ export default function Seance() {
 
   return (
     <Layout pageTitle={"Séance"}>
-      <WhiteBurger />
+      <WaveWhiteBurger />
       <div className="flex flex-col pl-10 pr-10 pb-5">
         <h1 className={`${styles.title} mb-8`}>Séance</h1>
         <SeanceDetails id={id} />
         <AssociatedClients />
         <div className="flex item-center justify-center">
           <Link href="/seances/id/edit">
-            <button className={styles.add_btn}>Modifier</button>
+            <button
+              className={styles.add_btn}
+              style={{ opacity: "0.5" }}
+              disabled
+            >
+              Modifier
+            </button>
           </Link>
         </div>
       </div>
