2551f24cd44678cc23a4e1fe11545da8f25789a5
ChrichriR <chriss_s@hotmail.fr>
2022-06-27 10:49:11 +0200

    make the route for the create seance's button in the menu

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -16,9 +16,11 @@ export default function MobileMenu() {
           alt="votre photo"
           sx={{ width: 100, height: 100 }}
         />
-        <button className={style.button} style={{ opacity: "0.5" }}>
-          Créer une séance
-        </button>
+        <Link href="/seances/createFromMenu">
+          <a>
+            <button className={style.button}>Créer une séance</button>
+          </a>
+        </Link>
       </div>
       <div className={style.links}>
         <Link data-cy="dashboardBtn" href="/dashboard">
---
+++ pages/seances/createFromMenu.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import styles from "../../styles/Seances.module.css";
+import MainDataForm from "../../components/SeanceForm/MainDataForm";
+import UploadMediaForm from "../../components/SeanceForm/UploadMediaForm";
+import DescriptionForm from "../../components/SeanceForm/DescriptionForm";
+import ProgressStepper from "../../components/ProgressStepper/ProgressStepper";
+import { CreateSeanceContextProvider } from "../../context/createSeanceContext";
+import WhiteBurger from "../../components/WhiteBurger/WhiteBurger";
+import AddClientsForm from "../../components/SeanceForm/AddClientsForm";
+import Summary from "../../components/SeanceForm/Summary";
+
+export default function CreateSeance() {
+  const [activeStep, setActiveStep] = useState(0);
+
+  return (
+    <CreateSeanceContextProvider>
+      <Layout pageTitle="Création d'une séance" shape={true}>
+        <WhiteBurger />
+        <div>
+          <div className={styles.createContainer}>
+            <h1 className={styles.create_title}>Création de la séance</h1>
+
+            {activeStep === 0 && <UploadMediaForm />}
+            {activeStep === 1 && <MainDataForm />}
+            {activeStep === 2 && <DescriptionForm />}
+            {activeStep === 3 && <AddClientsForm />}
+            {activeStep === 4 && <Summary />}
+
+            <ProgressStepper
+              activeStep={activeStep}
+              setActiveStep={setActiveStep}
+              className={styles.progressStepper}
+            />
+          </div>
+        </div>
+      </Layout>
+    </CreateSeanceContextProvider>
+  );
+}
