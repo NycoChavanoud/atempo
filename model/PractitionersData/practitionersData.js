10540e31e9eae90d2355a3744e710eed72d8a0b8
Dervieux <dervieux.ben@gmail.com>
2022-07-05 11:26:49 +0200

    resolution conflict

--- model/PractitionersData/practitionersData.js
+++ model/PractitionersData/practitionersData.js
@@ -3,12 +3,13 @@ import { child, get, ref } from "firebase/database";
 
 export async function getAllPractitionersData() {
   const user = auth.currentUser;
-
   if (user) {
     try {
       const snapshot = await get(child(ref(db), `/practitioners/${user.uid}`));
       if (snapshot.exists()) {
         return snapshot.val();
+      } else {
+        console.log("No data available");
       }
     } catch (error) {
       console.error(error);
