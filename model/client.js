2be74ff07016523de8a078e0c5f2b3ed4e51f372
ChrichriR <chriss_s@hotmail.fr>
2022-07-06 15:44:17 +0200

    pagination for the clients

--- model/client.js
+++ model/client.js
@@ -1,4 +1,13 @@
-import { ref, set, child, get, update } from "firebase/database";
+import {
+  ref,
+  set,
+  child,
+  get,
+  update,
+  startAt,
+  endAt,
+  orderByChild,
+} from "firebase/database";
 import { db, auth } from "../config/firebaseConfig";
 import uniqid from "uniqid";
 
@@ -6,10 +15,12 @@ const user = auth.currentUser;
 
 export async function createClient(clientData) {
   const id = uniqid();
+  const creation_date = Date.now();
   if (user) {
     await set(ref(db, `clients/${user.uid}/${id}`), {
       ...clientData,
       id,
+      creation_date,
     }).catch((error) => {
       console.log("Une erreur est survenue lors de l'enregistrement.") + error;
     });
@@ -35,17 +46,24 @@ export async function getClientData(clientId) {
 }
 
 export async function updateClient(clientId, data) {
+  const last_update = Date.now();
   if (user) {
     update(ref(db, `clients/${user.uid}/${clientId}`), {
       ...data,
+      last_update,
     });
   }
 }
 
-export async function getClientList() {
+export async function getClientList(page = 0) {
   if (user) {
     try {
-      const snapshot = await get(child(ref(db), `clients/${user.uid}`));
+      const snapshot = await get(
+        child(ref(db), `clients/${user.uid}`),
+        orderByChild("creation_date"),
+        startAt(page * 6),
+        endAt(page * 6 + 6)
+      );
       if (snapshot.exists()) {
         const showClient = Object.keys(snapshot.val()).map(
           (client) => snapshot.val()[client]
