6913b0d14bd3d31e54b6ce56ef915306a0b6c194
Dervieux <dervieux.ben@gmail.com>
2022-07-11 17:09:32 +0200

    modification for admin panel

--- pages/adminMenu/index.js
+++ pages/adminMenu/index.js
@@ -3,7 +3,7 @@ import WaveAdminCross from "../../components/WaveAdminCross/WaveAdminCross";
 import Link from "next/link";
 import Image from "next/image";
 import Layout from "../../components/Layout/Layout";
-import Account from "../../components/Account/Account";
+import Account from "../../../components/Account/Account";
 
 export default function admin() {
   return (
