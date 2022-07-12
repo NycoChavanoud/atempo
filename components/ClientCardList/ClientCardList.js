9ee90df9b4319b1bc586b4347f3a8a6b6d22e811
ChrichriR <chriss_s@hotmail.fr>
2022-07-12 14:14:39 +0200

    code more clean

--- components/ClientCardList/ClientCardList.js
+++ components/ClientCardList/ClientCardList.js
@@ -1,14 +1,32 @@
 import style from "./ClientCardList.module.css";
 import ClientCard from "../ClientCard/ClientCard";
+import { getClientList } from "../../model/client.js";
+import { useEffect, useState } from "react";
 
-export default function ClientCardList({ clientList }) {
+export default function ClientCardList() {
+  const [clientList, setClientList] = useState([]);
+
+  useEffect(() => {
+    getClientList().then(setClientList);
+  }, []);
+
   return (
-    <div className={style.center}>
-      <div className={style.list}>
-        {clientList.map((c) => (
-          <ClientCard key={c.id} id={c.id} />
-        ))}
+    <>
+      <div className={style.search}>
+        <input
+          type="text"
+          className={style.input}
+          id="lastname"
+          placeholder="Rechercher par nom."
+        />
       </div>
-    </div>
+      <div className={style.center}>
+        <div className={style.list}>
+          {clientList.map((c) => (
+            <ClientCard key={c.id} id={c.id} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -5,18 +5,10 @@ import DesktopMenu from "../../components/DesktopMenu/DesktopMenu";
 import Link from "next/link";
 import Avatar from "../../components/Avatar/Avatar";
 import ClientCardList from "../../components/ClientCardList/ClientCardList";
-import React, { useState, useEffect } from "react";
-import { getClientList } from "../../model/client.js";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Clients() {
-  const [clientList, setClientList] = useState([]);
-
-  useEffect(() => {
-    getClientList().then(setClientList);
-  }, []);
-
   return (
     <Layout pageTitle="Mes Clients">
       <div className={style.boxes}>
@@ -32,8 +24,7 @@ export default function Clients() {
               </div>
               <h1 className={style.title}>Mes clients</h1>
             </div>
-
-            <ClientCardList clientList={clientList} />
+            <ClientCardList />
             <div className={style.container}>
               <Link href="/clients/creationClient">
                 <button className={style.btn}>Ajouter une fiche client</button>
