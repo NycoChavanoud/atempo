e9b74b07939a35cf8366573735454333bd2c1740
fflachat <florent.flachat@gmail.com>
2022-07-25 11:57:30 +0200

    -

--- components/DisplayCurrentUser/DisplayCurrentUser.js
+++ components/DisplayCurrentUser/DisplayCurrentUser.js
@@ -2,9 +2,11 @@ import style from "../DisplayCurrentUser/DisplayCurrentUser.module.css";
 import { useEffect } from "react";
 import { getAllPractitionersData } from "../../model/PractitionersData/practitionersData";
 import { useState } from "react";
+import { useAuth } from "../../context/authContext";
 
 const DisplayCurrentUser = () => {
   const [practitionersData, setPractitionersData] = useState("");
+  const { user } = useAuth();
 
   useEffect(() => {
     getAllPractitionersData(user).then(setPractitionersData);
